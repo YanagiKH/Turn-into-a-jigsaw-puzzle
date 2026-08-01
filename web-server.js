97d8a23511bcac5893e17bfec32b079d8cc737cc
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-08-01 16:47:30 +0900

    Add local web server

---
+++ web-server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const mime = {
+  '.html': 'text/html; charset=utf-8',
+  '.js': 'application/javascript; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.json': 'application/manifest+json; charset=utf-8',
+  '.svg': 'image/svg+xml',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon'
+};
+
+http.createServer((req, res) => {
+  const url = (req.url || '/').split('?')[0];
+  const rel = url === '/' ? '/index.html' : url;
+  const filePath = path.join(__dirname, rel);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+      res.end('Not found');
+      return;
+    }
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
+}).listen(4173, () => {
+  console.log('Web preview on http://localhost:4173');
+});
