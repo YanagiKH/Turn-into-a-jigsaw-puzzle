85b0dbfba9640fbce765b0033a3477c352faa057
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-08-01 17:47:47 +0900

    Expose mod APIs to renderer

--- preload.js
+++ preload.js
@@ -2,5 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('puzzleApi', {
   saveProject: (suggestedName, content) => ipcRenderer.invoke('puzzle:save-project', { suggestedName, content }),
-  loadProject: () => ipcRenderer.invoke('puzzle:load-project')
+  loadProject: () => ipcRenderer.invoke('puzzle:load-project'),
+  listMods: () => ipcRenderer.invoke('puzzle:list-mods'),
+  readModFile: (filePath) => ipcRenderer.invoke('puzzle:read-mod-file', { filePath }),
+  getModRoots: () => ipcRenderer.invoke('puzzle:get-mod-roots')
 });
