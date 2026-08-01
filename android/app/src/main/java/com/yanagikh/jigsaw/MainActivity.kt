9c5bb6b67b233a7ed810979f1520d5aff78a326e
KH. <142028490+YanagiKH@users.noreply.github.com>
2026-08-01 17:07:07 +0900

    Add Android main activity

---
+++ android/app/src/main/java/com/yanagikh/jigsaw/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.yanagikh.jigsaw
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            allowFileAccess = true
+            allowContentAccess = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            mediaPlaybackRequiresUserGesture = false
+            useWideViewPort = true
+            loadWithOverviewMode = true
+            builtInZoomControls = true
+            displayZoomControls = false
+        }
+        webView.setBackgroundColor(0xFF0E1117.toInt())
+        webView.loadUrl("file:///android_asset/web/index.html")
+    }
+}
